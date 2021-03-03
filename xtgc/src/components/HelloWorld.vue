f493a45011bdf81d3608456b2a4d2f82da7130d4
wangxukun <819259283@qq.com>
2021-03-03 11:00:48 +0800

    Update HelloWorld.vue
    gg

--- xtgc/src/components/HelloWorld.vue
+++ xtgc/src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      For this program auto deploy with jenkins. This code is managed by git hub! View this project,<br>
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <a href="http://www.sakurafly.icu:9000/" target="_blank" rel="noopener">Jenkins Program</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
